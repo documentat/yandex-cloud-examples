f404c6cfd884e8c216d2a8f7de27f9a8854fa022
anatolykraev <anatolykraev@yandex-team.ru>
2022-06-01 18:17:55 +0300

    YCDOCIO-1476: changes after review

--- tutorials/terraform/sqlserver-data-migration.tf
+++ tutorials/terraform/sqlserver-data-migration.tf
@@ -3,56 +3,57 @@
 # RU: https://cloud.yandex.ru/docs/managed-sqlserver/tutorials/data-migration
 # EN: https://cloud.yandex.com/en/docs/managed-sqlserver/tutorials/data-migration
 #
-# Set the configuration of Managed Service for SQL Server:
-#      * DBMS version and edition
-#      * database name
-#      * name and password of the database owner user
-
-# Network
-resource "yandex_vpc_network" "network" {
-  name        = "network"
-  description = "Network for Managed Service for SQL Server."
+# Set the configuration of the Managed Service for SQL Server cluster:
+locals {
+  sql_server_version = "" # Set the SQL Server version. Мust be the same or higher than the version in the source cluster.
+  db_name            = "" # Set a database name
+  username           = "" # Set a user name
+  password           = "" # Set a user password
 }
 
-# Subnet in ru-central1-a availability zone
+resource "yandex_vpc_network" "network" {
+  description = "Network for the Managed Service for SQL Server cluster"
+  name        = "network"
+}
+
 resource "yandex_vpc_subnet" "subnet-a" {
+  description    = "Subnet in ru-central1-a availability zone"
   name           = "subnet-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.1.0.0/16"]
 }
 
-# Security group for Managed Service for SQL Server
-resource "yandex_vpc_default_security_group" "security-group" {
-  network_id = yandex_vpc_network.network.id
+resource "yandex_vpc_security_group" "security-group" {
+  description = "Security group for the Managed Service for SQL Server cluster"
+  network_id  = yandex_vpc_network.network.id
 
-  # Allow connections to SQL Server cluster from the Internet
   ingress {
-    protocol       = "TCP"
     description    = "Allow connections to SQL Server from the Internet"
+    protocol       = "TCP"
     port           = 1433
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    protocol       = "ANY"
     description    = "Allow outgoing connections to any required resource"
+    protocol       = "ANY"
     from_port      = 0
     to_port        = 65535
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Managed Service for SQL Server
 resource "yandex_mdb_sqlserver_cluster" "sqlserver-cluster" {
+  description        = "Managed Service for SQL Server cluster"
   name               = "sqlserver-cluster"
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.network.id
-  version            = "" # Set DBMS version and edition
-  security_group_ids = [yandex_vpc_default_security_group.security-group.id]
+  version            = local.sql_server_version
+  security_group_ids = [yandex_vpc_security_group.security-group.id]
 
   resources {
-    resource_preset_id = "s2.small"
+    resource_preset_id = "s2.small" # 4 vCPU, 16 GB RAM
     disk_type_id       = "network-hdd"
     disk_size          = 10 # GB
   }
@@ -60,19 +61,19 @@ resource "yandex_mdb_sqlserver_cluster" "sqlserver-cluster" {
   host {
     zone             = "ru-central1-a"
     subnet_id        = yandex_vpc_subnet.subnet-a.id
-    assign_public_ip = true
+    assign_public_ip = true # Required for connection from the Internet
   }
 
   database {
-    name = "" # Set the database name
+    name = local.db_name
   }
 
   user {
-    name     = "" # Set the name of the database owner user
-    password = "" # Set the password of the database owner user
+    name     = local.username
+    password = local.password
 
     permission {
-      database_name = "" # Database name
+      database_name = local.db_name
       roles         = ["OWNER"]
     }
   }
